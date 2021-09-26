fba10dfbbe2f7a9a83af0f73e57268c139ba19ef
Sharath <sharath.esr@gmail.com>
2021-09-27 01:40:22 +0530

    done and added style

--- app.js
+++ app.js
@@ -2,10 +2,30 @@ var btnTranslate = document.querySelector("#btn-Translate");
 var txt = document.querySelector("#txt-Id");
 var div_var = document.querySelector("#output");
 
-function clickEventHandler()
-{
-    div_var.innerText = txt.value;
-    
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text
 }
 
-btnTranslate.addEventListener("click",clickEventHandler);
+function errorhandler(error) {
+  console.log("error occured", error);
+}
+
+
+function clickEventHandler() {
+  var inputText = txt.value; //taking input
+
+  //calling server for processing
+  fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+      var translatedText = json.contents.translated;
+      div_var.innerText = translatedText;
+    }).catch(errorhandler);
+};
+
+btnTranslate.addEventListener("click", clickEventHandler);
